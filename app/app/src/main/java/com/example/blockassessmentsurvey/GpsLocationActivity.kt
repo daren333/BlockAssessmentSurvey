9e86210837ef9f7723613d1df1f8da159e765851
Daren Ansher <dansher@Darens-MBP.fios-router.home>
2019-10-28 21:08:47 -0400

    added gps location activity

--- app/app/src/main/java/com/example/blockassessmentsurvey/GpsLocationActivity.kt
+++ app/app/src/main/java/com/example/blockassessmentsurvey/GpsLocationActivity.kt
@@ -1,13 +1,22 @@
 package com.example.blockassessmentsurvey
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.telephony.TelephonyManager
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import kotlinx.android.synthetic.main.activity_get_gps_location.*
 
@@ -21,6 +30,8 @@ class GpsLocationActivity : AppCompatActivity() {
     private lateinit var addrText: EditText
     private lateinit var button: Button
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -33,38 +44,75 @@ class GpsLocationActivity : AppCompatActivity() {
         // Set content view
         setContentView(R.layout.activity_get_gps_location)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         // Initialize UI elements
-        addrText = findViewById(R.id.location)
+
         button = findViewById(R.id.mapButton)
 
         // Link UI elements to actions in code
-        button.setOnClickListener { processClick() }
+        button.setOnClickListener { loadAddress() }
 
     }
 
-    // Called when user clicks the Show Map button
 
-    private fun processClick() {
+
+    private fun loadAddress() {
+        Log.i(TAG, "loadAddress() called")
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            Log.i(TAG, "Permission denied")
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+
         try {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : android.location.Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    val latitude = location!!.latitude.toString()
+                    val longitude = location!!.longitude.toString()
+                    Log.i(TAG, "Latitude: $latitude")
+                    Log.i(TAG, "Longitude: $longitude")
+                    // Create Intent object for starting Google Maps application
+                    /*val geoIntent = Intent(
+                        Intent.ACTION_VIEW, Uri
+                            .parse("geo:$latitude, $longitude"))
 
-            // Process text for network transmission
-            var address = addrText.text.toString()
-            address = address.replace(' ', '+')
+                    if (packageManager.resolveActivity(geoIntent, 0) != null) {
+                        // Use the Intent to start Google Maps application using Activity.startActivity()
+                        startActivity(geoIntent)
+                    }*/
 
-            // Create Intent object for starting Google Maps application
-            val geoIntent = Intent(
-                Intent.ACTION_VIEW, Uri
-                    .parse("geo:0,0?q=$address"))
-
-            if (packageManager.resolveActivity(geoIntent, 0) != null) {
-                // Use the Intent to start Google Maps application using Activity.startActivity()
-                startActivity(geoIntent)
-            }
-
+                }
         } catch (e: Exception) {
             // Log any error messages to LogCat using Log.e()
             Log.e(TAG, e.toString())
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        Log.i(TAG, "onRequestPermissionsResult called")
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    loadAddress()
+
+                } else {
+
+                    Log.i(TAG, "Address not loaded --- Permission was not granted")
+
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
     }
 
 
