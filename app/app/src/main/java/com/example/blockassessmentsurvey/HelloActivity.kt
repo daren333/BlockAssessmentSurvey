0d74ac6befc4cfca8a1388ef5728447057cab699
jgill123 <jgill123@umd.edu>
2019-10-29 16:49:18 -0400

    started survey manager

--- app/app/src/main/java/com/example/blockassessmentsurvey/HelloActivity.kt
+++ app/app/src/main/java/com/example/blockassessmentsurvey/HelloActivity.kt
@@ -1,12 +1,21 @@
 package com.example.blockassessmentsurvey
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HelloActivity : AppCompatActivity() {
+    private lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
+
+        val intent = intent
+        val textToShow = intent.getStringExtra("text")
+        text = findViewById(R.id.textView)
+        if(textToShow != null){
+            text.text = textToShow
+        }
     }
 }
--- app/app/src/main/java/com/example/blockassessmentsurvey/SurveyManager.kt
+++ app/app/src/main/java/com/example/blockassessmentsurvey/SurveyManager.kt
@@ -1,28 +1,68 @@
 package com.example.blockassessmentsurvey
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class SurveyManager : AppCompatActivity() {
-    private lateinit var databaseQuestions: DatabaseReference
+    private lateinit var database: DatabaseReference
+    private lateinit var databaseSurveys: DatabaseReference
+    private lateinit var survey1Btn: Button
+    private lateinit var survey2Btn: Button
+    private lateinit var surveyQuestions: MutableList<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey_manager)
 
-        //get survey value from extra
-        val intent = intent
-        val survey = intent.getStringExtra(SURVEY_STRING)
-
         //get reference to database
-        databaseQuestions = FirebaseDatabase.getInstance().getReference("surveys").child(survey)
+        database = FirebaseDatabase.getInstance().reference
+        databaseSurveys = FirebaseDatabase.getInstance().getReference("surveys")
 
+        //initialize views
+        initializeViews()
+
+        //depending on which button is pushed, query the database to get relevant questions
+        survey1Btn.setOnClickListener {
+            getSurveyQuestions(SURVEY1_STRING)
+        }
+        survey2Btn.setOnClickListener {
+            getSurveyQuestions(SURVEY2_STRING)
+        }
+    }
+
+    private fun getSurveyQuestions(survey: String){
+        var questions = databaseSurveys.child(survey)
+        val intent = Intent(this@SurveyManager, HelloActivity::class.java)
+        intent.putExtra("text", questions.child("q1").child("q1a").toString())
+        startActivity(intent)
+    }
+
+    private fun initializeViews(){
+        survey1Btn = findViewById(R.id.survey1_button)
+        survey2Btn = findViewById(R.id.survey2_button)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        databaseSurveys.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "Data changed")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
     }
 
     companion object {
-        private val SURVEY_STRING = "surveystring"
+        private val SURVEY1_STRING = "survey1"
+        private val SURVEY2_STRING = "survey2"
         private val QUESTION_STRING = "questionstring"
         private val TAG = "BAS-SurveyManager"
     }
