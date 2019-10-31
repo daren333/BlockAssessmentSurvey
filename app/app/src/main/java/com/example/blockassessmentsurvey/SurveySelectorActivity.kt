db5b2032176001cec33a0d5ca67e3509ea88779f
Daren Ansher <dansher@wireless-10-105-171-4.umd.edu>
2019-10-31 13:17:40 -0400

    added icons

---
+++ app/app/src/main/java/com/example/blockassessmentsurvey/SurveySelectorActivity.kt
@@ -0,0 +1,13 @@
+package com.example.blockassessmentsurvey
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SurveySelectorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey_selector)
+    }
+}
