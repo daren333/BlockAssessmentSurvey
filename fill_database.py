847b06093246532d923d5afa480131f6db129a2d
Daren Ansher <dansher@Darens-MBP.fios-router.home>
2019-11-05 22:18:05 -0500

    fill_db script almost done

--- fill_database.py
+++ fill_database.py
@@ -1,56 +1,91 @@
-import csv
+import json
+import jsonpickle as jp
 import re
+from firebase import firebase
 
 class Question:
 
-    def __init__(self, q_num, q_text, answer_type, answers, skip_logic):
-        self.q_num = q_num
-        self.q_text = q_text
-        self.answer_type = answer_type
-        self.answers = answers
+    def __init__(self, q_num, q_text, answer_type, answers, skip_logic, survey_num):
+        self.questionNum = q_num
+        self.questionText = q_text
+        self.questionType = answer_type
+        self.answer = answers
         self.skip_logic = skip_logic
+        self.survey_num = survey_num
 
     def to_string(self):
-        return self.q_text + " " + self.answer_type + " " + self.answers + " " + self.skip_logic + "\n"
+        return str(self.q_num) + " " + self.q_text + " " + self.answer_type + " " + self.answers + " " + self.skip_logic + "\n"
+
+    def to_json(self):
+        #return "{\"qId\": \"{}\", \"questionText\" : \"{}\", \"questionType\": \"{}\", \
+         #            \"answer\": \"{}\", \"skipLogic\": \"{}\", \"surveyNumber\": \"{}\"\}" \
+          #           .format(str(self.questionNum), self.questionText, self.questionType, self.answer, self.skip_logic, str(self.survey_num))
+       return "{\n\"qId\": " + str(self.questionNum) + ",\n\"questionText\": \"" + self.questionText + "\",\n\"questionType\": \"" + self.questionType \
+           + "\",\n\"answer\": \"" + self.answer + "\", \"skipLogic\": \""+ self.skip_logic + "\",\n\"surveyNumber\": " + str(self.survey_num) + "\n},\n"
 
 class Survey:
     questions = []
 
-    def __init__(self, id):
+    def __init__(self, id, title):
         self.id = id
+        self.title = title
 
-    def add(self, question):
+    def add_question(self, question):
         self.questions.append(question)
 
-def is_header(line):
-    if line[0] == "Question" and line[1] == "Answer type" and line[2] == "Answers" and line[3] == "Skip logic":
-        return True
-    else:
-        return False
+    def print_questions(self):
+        print("Survey %d. %s\n", self.id, self.title)
+        for q in self.questions:
+            print(q.to_string())
 
-csv_file = open('/Users/dansher/Documents/UMD/courses/fall 2019/Handheld Programming/BlockAssessmentSurvey/questions_list.csv')
+
+q_file = open('/Users/dansher/Documents/UMD/courses/fall 2019/Handheld Programming/BlockAssessmentSurvey/qlist.tsv')
 flag = 0
 counter = 0
 survey_num = 0
 surveys = []
+questions = []
     
-q_header = ["Question", "Answer type", "Answers", "Skip logic"]
-csv_reader = csv.reader(csv_file, delimiter=',')
-for line in csv_reader:
-    if re.search("Table \d", str(line)):
-        survey_num += 1
-        counter = 1
-        s = Survey(survey_num)
-        surveys.append(s)
-    elif survey_num > 0 and line != q_header and line:
-        q = Question(counter, line[0], line[1], line[2], line[3])
-        s.add(q)
-        counter += 1
+tables = re.split(r"Table \d\.", q_file.read())
 
+# Get rid of first four tables (0a, 0b, 0c and Exit Menu)
+for i in range(0, 3):
+    tables.pop(0)
+# Capture title then get rid of first three lines
+for table in tables:
+    curr_id = 1
+    q_list = table.split('\n')
+    title = q_list[0]
+    for i in range(0, 3):
+        q_list.pop(0)
+
+    # Create new survey
+    s = Survey(curr_id, title)
+    survey_num += 1
+
+    # Split questions by comma (max set to three splits so if comma in skip logic it shouldn't cause an issue)
+    # Create new question object and add it to survey's list of questions
+    q_num = 1
+    for q in q_list:
+        q_parts = re.split('\t', q, 3)
+        if len(q_parts) > 3 and q_parts[0]:
+            questions.append((Question(q_num, q_parts[0], q_parts[1], q_parts[2], q_parts[3], survey_num)))
+            q_num += 1
+
+    # Add survey into list of surveys
+    surveys.append(s)
+
+firebase = firebase.FirebaseApplication('https://blockassessmentsurvey.firebaseio.com/', None)
+s = questions[0].to_json()
+result = firebase.post('/questions', questions[0].qId, s)
+print(result)
 
 f = open("qs_test.txt","w") 
-
-for s in surveys:   
-    f.write("Survey # %d\n" % s.id)
-    for q in s.questions:
-        f.write(q.to_string())
+#for s in surveys:
+for q in questions:
+        #x = q.isoformat()
+    
+    f.write(q.to_json())
+        #x = json.dumps(q, cls=MyEncoder)
+        #f.write(x)
+    #s.print_questions()
