fdd2d92f7d4772e5b0b10da582b6f2b178361383
Daren Ansher <dansher@wireless-10-105-175-239.umd.edu>
2019-11-09 18:36:17 -0500

    clickable pictures ftw

--- app/app/src/main/java/com/example/blockassessmentsurvey/HelloActivity.kt
+++ app/app/src/main/java/com/example/blockassessmentsurvey/HelloActivity.kt
@@ -1,6 +1,7 @@
 package com.example.blockassessmentsurvey
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HelloActivity : AppCompatActivity() {
@@ -8,5 +9,11 @@ class HelloActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
+
+        val survey = intent.getStringExtra("surveyType")
+        val tv : TextView = findViewById(R.id.textView1)
+        tv.text = survey
+
+
     }
 }
